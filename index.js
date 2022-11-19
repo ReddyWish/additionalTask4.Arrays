a53abd75b35e7fdcff46e467effd8bda6f4b1262
Ilya <ilya.shuvatov1@gmail.com>
2022-11-19 10:31:07 +0100

    second commit

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const matrix = [
     [ 7, 8, 9 ],
 ]
 
-let firstEl = matrix[0]
-let secondEl = matrix[1]
-let thirdEl = matrix[2]
+// let firstEl = matrix[0]
+// let secondEl = matrix[1]
+// let thirdEl = matrix[2]
+
+// let [firstEl, secondEl, thirdEl] = matrix
+// let newMatrix = []
+//
+// newMatrix = newMatrix.concat(firstEl, secondEl, thirdEl)
+// console.log(newMatrix)
 
 let newMatrix = []
-newMatrix = newMatrix.concat(firstEl, secondEl, thirdEl)
+matrix.forEach(item => {
+    if (Array.isArray(item)) {
+       newMatrix = newMatrix.concat(...item)
+    }
+    else newMatrix = newMatrix.concat(item)
+})
 console.log(newMatrix)
+console.log(matrix)
+
